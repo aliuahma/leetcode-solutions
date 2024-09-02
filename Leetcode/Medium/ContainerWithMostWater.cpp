c075bcc4e56852fc89806e5482926ebe94443965
Ali Ahmad <57443539+aliuahma@users.noreply.github.com>
2024-09-02 15:45:50 +0200

    Update ContainerWithMostWater.cpp

--- Leetcode/Medium/ContainerWithMostWater.cpp
+++ Leetcode/Medium/ContainerWithMostWater.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        int maxArea = 0;
         int left = 0;
         int right = height.size() - 1;
-        int largest = 0;
-        while(left != right) {
-            int area = (height[left] < height[right] ? height[left] : height[right]) * (right - left);
-            largest = (area > largest) ? area : largest;
-            if(height[left] < height[right]) left++;
-            else right--;
+        while(left < right) {
+            maxArea = max(maxArea, min(height[left], height[right]) * (right - left));
+            if(height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            } 
         }
-        return largest;
+        return maxArea;
     }
 };
